28dd60a5b05a3c31ce9382b7973fb2419d866d88
youssef <yberrakouan.lol@gmail.com>
2024-12-21 12:43:03 +0100

    fixing :Check for Dynamic Rendering with Props of UserCard component
    pages/users/index.tsx doesn't contain: [posts.map]

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -33,10 +33,11 @@ const Users: React.FC<UsersProps> = ({ posts }) => {
       </button>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} />
-        ))}
-      </div>
+  {users.map((user) => (
+    <UserCard key={user.id} {...user} />
+  ))}
+    </div>
+
 
       {/* Render the UserModal component */}
       <UserModal
